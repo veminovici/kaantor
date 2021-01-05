2b8e8d924197d0f31942cc713cb96dea3f46ff36
VLAD EMINOVICI <veminovici@hotmail.com>
2021-01-05 18:45:00 +0200

    Add an example of serializing a function.

---
+++ tests/Scripts/q.fsx
@@ -0,0 +1,40 @@
+#r "nuget: FsPickler"
+#r "nuget: FsPickler.Json"
+#r "nuget: FSharp.Quotations.Evaluator"
+
+open MBrace.FsPickler
+open MBrace.FsPickler.Json
+
+open FSharp.Quotations
+open FSharp.Quotations.Evaluator
+
+open System.IO
+open System.Text
+
+let serializer = JsonSerializer(indent = true)
+let utf8 = UTF8Encoding(false)
+
+let toJson x =
+    use stream = new MemoryStream()
+    serializer.Serialize(stream, x)
+    stream.ToArray() |> utf8.GetString
+
+let parseJson<'a> json =
+    use reader = new StringReader(json)
+    serializer.Deserialize<'a>(reader)
+
+let fn x y = x + y + 1
+
+let f = <@ fn 10 @>
+let serialized = toJson f
+
+printfn "Serialized:\n%s" serialized
+
+let deserialized = parseJson<Expr<int -> int>> serialized
+
+let increment = deserialized |> QuotationEvaluator.Evaluate
+
+1
+|> increment
+|> printfn "Res: %d"
+ 
