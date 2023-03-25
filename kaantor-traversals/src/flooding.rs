ac3c2e8e860b0142d7e8aa9d7be8787bc994076a
Vlad Eminovici <veminovici@hotmail.com>
2023-03-25 20:41:50 +0200

    Remove the unused test

--- kaantor-traversals/src/flooding.rs
+++ kaantor-traversals/src/flooding.rs
@@ -66,29 +66,3 @@ impl Handler<ProtocolMsg<FloodingPld>> for FloodingNode {
     }
 }
 
-#[cfg(test)]
-mod utests {
-    use super::*;
-    use kaantor::IntoActorId;
-
-    #[test]
-    fn protocol() {
-        env_logger::init();
-        log::info!("Starting the example NEXUS_GET");
-
-        // initialize system
-        let _code = System::new().block_on(async {
-            // STEP 1: Create the nodes
-            let node1 = FloodingNode::build(1.into()).await;
-            let node2 = FloodingNode::build(2.into()).await;
-            let node3 = FloodingNode::build(3.into()).await;
-
-            // STEP 2: Create the edges between the nodes
-            let _ = nexus::add_edge::<FloodingPld>(node1.aid(), node2.aid()).await; // 1 - 2
-            let _ = nexus::add_edge::<FloodingPld>(node1.aid(), node3.aid()).await; // 1 - 3
-
-            // STEP 3: Start the protocol
-            let _ = node1.send(10.into(), FloodingPld::Start(12)).await;
-        });
-    }
-}
